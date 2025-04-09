97e3392844bbbea3ad271b879ae069d3158eeb89
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2025-04-09 10:33:26 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 105 ms, Memory: 27,520 KB -BaekjoonHub

---
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 활주로 건설 - 4014 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWIeW7FakkUDFAVH) 
+
+### 성능 요약
+
+메모리: 27,520 KB, 시간: 105 ms, 코드길이: 1,812 Bytes
+
+### 제출 일자
+
+2025-04-09 10:33
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		for(int o = 1; o<=tc; o++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			int[][] arr = new int[N][N];
+			for(int i = 0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j<N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int sum = 0;
+			int count = 0;
+			int height = 0;
+			boolean canMake = false;
+			sum += cal(true,arr,N,X);
+			sum += cal(false,arr,N,X);
+			System.out.println("#"+o+" "+sum);
+		}
+	}
+	public static int cal(boolean rc,int[][] arr,int N,int X) {
+		int sum = 0;
+		int count = 0;
+		int height = 0;
+		boolean canMake = false;
+		Outer : for(int row = 0; row<N; row++) {
+			height = rc ? arr[row][0] : arr[0][row];
+			count = 1;
+			canMake = false;
+			for(int col = 1; col<N; col++) {
+				if(height==(rc ? arr[row][col] : arr[col][row])) {
+					count+=1;
+					if(canMake&&count==X) {
+						canMake = false;
+						count = 0;
+					}
+				}else if((rc ? arr[row][col] : arr[col][row])==height+1) {
+					if(canMake) continue Outer;
+					if(count<X) {
+						continue Outer;
+					}else {
+						count = 1;
+						height = height+1;
+					}
+				}else if((rc ? arr[row][col] : arr[col][row])==height-1) {
+					if(canMake) continue Outer;
+					canMake = true;
+					count = 1;
+					height = height-1;
+				}else {
+					continue Outer;
+				}
+			}
+			if(!canMake) sum+=1;
+		}
+		return sum;
+	}
+
+}
