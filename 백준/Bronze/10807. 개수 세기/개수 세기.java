cbc3993cef015a37f3e091d8b728de3493c1ae25
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-01-27 13:27:24 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            if (numbers[i] == v) answer++;
+        }
+        System.out.println(answer);
+    }
+}
