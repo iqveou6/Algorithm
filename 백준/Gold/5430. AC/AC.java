de0735dc1a027dfb733da53623e58ae8f338874c
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-02-09 12:39:43 +0900

    [Gold V] Title: AC, Time: 492 ms, Memory: 70036 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,67 +1,59 @@
-
-
-import java.util.List;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-		for (int i = 0; i < t; i++) {
-			String p = br.readLine();
-			int n = Integer.parseInt(br.readLine());
-			String x = br.readLine();
-			String[] arr = x.substring(1, x.length()-1).split(",");
-			
-			List<String> list = new ArrayList<>();
-			for (int j = 0; j < n; j++) {
-				list.add(arr[j]);
-			}
-			
-			int reverse = 0;
-			StringBuilder sb = new StringBuilder();
-			sb.append("[");
-			boolean temp = true;
-			for(char c : p.toCharArray()) {
-				if (c == 'R') {
-					reverse += 1;
-				} else {
-					if (list.size() == 0) {
-						temp = false;
-						break;
-					}
-					if (reverse % 2 == 1) {
-						list.remove(list.size()-1);
-					} else {
-						list.remove(0);
-					}
-				}
-			}
-			if (temp) {
-				if (list.size()!=0) {
-					if (reverse %2 == 1) {
-						for(int k = list.size()-1; k > 0; k--) {
-							sb.append(list.get(k)).append(",");
-						}
-						sb.append(list.get(0));
-					}
-					else {
-						for(String s : list.subList(0, list.size()-1)) {
-							sb.append(s).append(",");
-						}
-						sb.append(list.get(list.size()-1));
-					}
-				}
-				sb.append("]");
-				System.out.println(sb);
-			}
-			else {
-				System.out.println("error");
-			}
-			
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            String p = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                br.readLine();
+                if (p.contains("D")) sb.append("error").append('\n');
+                else sb.append("[]").append('\n');
+                continue;
+            }
+            String[] num = br.readLine().split(",");
+            num[0] = num[0].replace("[", "");
+            num[n - 1] = num[n - 1].replace("]", "");
+            int flag = 0;
+            boolean error = false;
+            int left = 0, right = n - 1;
+            for (char c : p.toCharArray()) {
+                if (left > right) {
+                    sb.append("error").append('\n');
+                    error = true;
+                    break;
+                }
+                if (c == 'R') {
+                    flag += 1;
+                } else if (c == 'D') {
+                    if (flag % 2 == 0) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+            if (!error) {
+                sb.append("[");
+                if (flag % 2 == 0) {
+                    for (int j = left; j <= right; j++) {
+                        if (j == right) sb.append(num[j]);
+                        else sb.append(num[j]).append(",");
+                    }
+                } else {
+                    for (int j = right; j >= left; j--) {
+                        if (j == left) sb.append(num[j]);
+                        else sb.append(num[j]).append(",");
+                    }
+                }
+                sb.append("]").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
