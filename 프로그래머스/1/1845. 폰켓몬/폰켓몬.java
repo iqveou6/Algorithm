d9532b2ec1ed9fc677b3ae41d99f86e1543920c8
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-04-30 15:19:21 +0900

    [level 1] Title: 폰켓몬, Time: 13.57 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        if (nums.length/2 > map.size()) {
+            answer = map.size();
+        } else {
+            answer = nums.length/2;
+        }
+        return answer;
+    }
+}
