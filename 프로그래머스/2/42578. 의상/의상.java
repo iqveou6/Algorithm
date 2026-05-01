4f4ccc5c4d0928070153be75a848e5093da0125d
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-05-01 16:58:16 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] s : clothes) {
+            map.put(s[1], map.getOrDefault(s[1], 0) + 1);
+        }
+        for (Integer i : map.values()) {
+            answer *= (i+1);
+        }
+        return answer-1;
+    }
+}
