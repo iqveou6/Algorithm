0c605cfbb0b808584653cc2eba737dfa25ac11d2
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-05-04 16:13:53 +0900

    [level 2] Title: H-Index, Time: 1.48 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        int n = citations.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (n - i <= citations[i]) {
+                answer = n - i;
+            }
+        }
+        return answer;
+    }
+}
