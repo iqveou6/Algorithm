6656b9f5527f4bea74a4b7681bbd9a25007fc1aa
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-08-13 14:11:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.81 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0)+1);
+        }
+        
+        for (int i = 0; i < completion.length; i++) {
+            map.put(completion[i], map.getOrDefault(completion[i], 0) - 1);
+        }
+        
+        for (Map.Entry<String, Integer> set : map.entrySet()) {
+            String name = set.getKey();
+            Integer count = set.getValue();
+            if (count > 0) {
+                answer = name;
+                break;
+            }
+        }
+        return answer;
+    }
+}
