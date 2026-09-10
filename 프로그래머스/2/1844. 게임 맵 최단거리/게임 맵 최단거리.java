83add3ac5b9869a02c82e8fb13d1dbe7fb7624f8
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-09-10 14:25:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.86 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,32 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int answer = -1;
-        Queue<int[]> queue = new ArrayDeque<>();
+        int answer = 0;
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        int x = maps.length;
-        int y = maps[0].length;
-        int[][] visited = new int[x][y];
+        int r = maps.length; int c = maps[0].length;
+        int[][] visited = new int[r][c];
+        Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{0, 0});
         visited[0][0] = 1;
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int[] cur = queue.poll();
             for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                if (nx < 0 || nx >= x || ny < 0 || ny >= y) {
+                int cx = cur[0] + dx[i];
+                int cy = cur[1] + dy[i];
+                if (cx < 0 || cx >= r || cy < 0 || cy >= c) {
                     continue;
                 }
-                if (visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
-                    queue.offer(new int[]{nx, ny});
+                if (maps[cx][cy] == 1 && visited[cx][cy] == 0) {
+                    visited[cx][cy] = visited[cur[0]][cur[1]] + 1;
+                    if (cx == r-1 && cy == c-1) 
+                        return visited[cx][cy];
+                    queue.offer(new int[]{cx, cy});
                 }
             }
         }
-        if (visited[x-1][y-1] != 0) {
-            answer = visited[x-1][y-1];
-        }
-        return answer;
+        
+        return -1;
     }
 }
