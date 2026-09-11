68001f3a022e923174c13c9f9ece4b93545fc7c3
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-09-11 15:09:16 +0900

    [level 2] Title: 주차 요금 계산, Time: 18.85 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,41 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        // 차량 번호 기준 오름차순 자동 정렬
-        Map<String, Integer> totalTime = new TreeMap<>();
-        Map<String, Integer> inTime = new HashMap<>();
-
-        for (String record : records) {
-            String[] input = record.split(" ");
-            String[] time = input[0].split(":");
-            int cur = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
-            String car = input[1];
-
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> recordMap = new HashMap<>();
+        Map<String, Integer> totalTimeMap = new TreeMap<>();
+        for (int i = 0; i < records.length; i++) {
+            String[] input = records[i].split(" ");
+            int hour = Integer.parseInt(input[0].split(":")[0]);
+            int min = Integer.parseInt(input[0].split(":")[1]);
             if (input[2].equals("IN")) {
-                inTime.put(car, cur);
+                recordMap.put(input[1], hour * 60 + min);
             } else {
-                totalTime.put(car, totalTime.getOrDefault(car, 0) + cur - inTime.remove(car));
+                totalTimeMap.put(input[1], 
+                                 totalTimeMap.getOrDefault(input[1], 0) + hour * 60 + min - recordMap.get(input[1]));
+                recordMap.remove(input[1]);
             }
         }
-
-        // 미출차 차량 23:59 (1439분) 일괄 처리
-        for (String car : inTime.keySet()) {
-            totalTime.put(car, totalTime.getOrDefault(car, 0) + 1439 - inTime.get(car));
-        }
-
-        // 요금 계산 및 반환 (TreeMap 순회 시 이미 차량 번호 오름차순 정렬 상태)
-        int[] answer = new int[totalTime.size()];
-        int idx = 0;
-        for (int time : totalTime.values()) {
-            if (time <= fees[0]) {
-                answer[idx++] = fees[1];
-            } else {
-                // 정수 올림 나눗셈: (A + B - 1) / B
-                int extraUnits = (time - fees[0] + fees[2] - 1) / fees[2];
-                answer[idx++] = fees[1] + extraUnits * fees[3];
+        if (recordMap.size() > 0) {
+            for (String key : recordMap.keySet()) {
+                totalTimeMap.put(key, 
+                                 totalTimeMap.getOrDefault(key, 0) + 23 * 60 + 59 - recordMap.get(key));
             }
         }
-
-        return answer;
+        for (String key : totalTimeMap.keySet()) {
+            int fee = 0;
+            if (totalTimeMap.get(key) <= fees[0]) {
+                answer.add(fees[1]);
+            } else {
+                answer.add(fees[1] + (int)Math.ceil((double)(totalTimeMap.get(key) - fees[0]) / fees[2]) * fees[3]);
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
