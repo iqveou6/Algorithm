629f83889cd5e04b839fc50df53768995d11b130
iqveou6 <85685999+iqveou6@users.noreply.github.com>
2026-09-11 15:16:10 +0900

    [level 2] Title: 더 맵게, Time: 665.26 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 128 MB, 시간: 1479.69 ms
+메모리: 140 MB, 시간: 665.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 02일 23:29:41
+2026년 09월 11일 15:16:07
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,21 +1,17 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int sco : scoville) {
-            pq.offer(sco);
+        for (int i = 0; i < scoville.length; i++) {
+            pq.offer(scoville[i]);
         }
-        
         while (!pq.isEmpty()) {
-            int sco1 = pq.poll();
-            if (sco1 >= K) break;
-            if (pq.isEmpty()) {
-                return -1;
-            }
-            int sco2 = pq.poll();
-            pq.offer(sco1 + sco2 * 2);
+            int a = pq.poll();
+            if (a >= K) return answer;
+            if (pq.isEmpty()) return -1;
+            int b = pq.poll();
+            pq.offer(a + (b * 2));
             answer++;
         }
         return answer;
